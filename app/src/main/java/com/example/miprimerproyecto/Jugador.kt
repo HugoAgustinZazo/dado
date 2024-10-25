7ab1f7156412f1010dd0ae25b777beb5d6405b42
Hugo <huguitronic17@gmail.com>
2024-10-25 21:48:21 +0200

    final

--- app/src/main/java/com/example/miprimerproyecto/Jugador.kt
+++ app/src/main/java/com/example/miprimerproyecto/Jugador.kt
@@ -1,5 +1,37 @@
 package com.example.miprimerproyecto
 
-class Jugador(val nombre: String,var puntuacion: Int, var numjugador: Int, var puntuacionronda: Int) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Jugador(val nombre: String,var puntuacion: Int, var numjugador: Int, var puntuacionronda: Int): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nombre)
+        parcel.writeInt(puntuacion)
+        parcel.writeInt(numjugador)
+        parcel.writeInt(puntuacionronda)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Jugador> {
+        override fun createFromParcel(parcel: Parcel): Jugador {
+            return Jugador(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Jugador?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.miprimerproyecto
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.renderscript.ScriptGroup.Binding
@@ -20,9 +21,7 @@ import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var ronda = 1
-    var jugador = 0
-    val maxRondas = 5
+    var maxRondas = 0
     var jugadores: ArrayList<Jugador> = ArrayList()
     var opcion = 0
 
@@ -44,8 +43,11 @@ class MainActivity : AppCompatActivity() {
             }
             binding.textView.setText("Elige el numero de jugadores")
             binding.textView.textSize = 18F
-            ocultar()
-            ocultarDado()
+            binding.textView5.setText("Elige el numero de rondas")
+            binding.textView5.textSize = 18F
+
+            //ocultar()
+            //ocultarDado()
 
             val numJugadores: List<Int> = listOf(2, 3, 4)
             var adapter: ArrayAdapter<Int> =
@@ -65,168 +67,22 @@ class MainActivity : AppCompatActivity() {
                 override fun onNothingSelected(parent: AdapterView<*>) {
                 }
             }
-
             val boton = binding.button
-            val botontirar = binding.button2
-            val botonplantarse = binding.button3
 
+            maxRondas = binding.editTextNumber.text.toString().toInt()
 
             boton.setOnClickListener {
                 for (i in 1..opcion) {
                     jugadores.add(Jugador("Jugador " + i, 0, i, 0))
                 }
-                Toast.makeText(
-                    this@MainActivity,
-                    "Numero de jugadores: $opcion",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-                binding.button.visibility = View.GONE
-                binding.spinner2.visibility = View.GONE
-                binding.textView.visibility = View.GONE
+                Toast.makeText(this@MainActivity,"Numero de jugadores: $opcion",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"Numero de rondas: $maxRondas",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, JugadoresActivity::class.java)
+                intent.putExtra("OPCION", opcion)
+                intent.putExtra("MAXRONDAS", maxRondas)
+                startActivity(intent)
 
-                mostrar()
-                binding.textView.textSize = 25F
-                binding.textView4.setText("Numero de Ronda: $ronda")
-                binding.textView4.textSize = 25F
-                binding.textView2.setText("Turno del " + jugadores[jugador].nombre)
-                binding.textView2.textSize = 18F
-                binding.imageView9.visibility = View.GONE
-
-            }
-            botontirar.setOnClickListener {
-                ocultarDado()
-                var numal: Int = Random.nextInt(1, 7)
-                when (numal) {
-                    1 -> binding.imageView3.visibility = View.VISIBLE
-                    2 -> binding.imageView4.visibility = View.VISIBLE
-                    3 -> binding.imageView5.visibility = View.VISIBLE
-                    4 -> binding.imageView6.visibility = View.VISIBLE
-                    5 -> binding.imageView7.visibility = View.VISIBLE
-                    6 -> binding.imageView8.visibility = View.VISIBLE
-
-
-                }
-                binding.textView4.setText("Numero de Ronda: $ronda")
-                binding.textView2.setText("Turno del " + jugadores[jugador].nombre)
-                binding.textView3.visibility = View.VISIBLE
-
-                if (numal == 1) {
-                    jugadores[jugador].puntuacionronda = 0
-                    binding.textView3.setText("El " + jugadores[jugador].nombre + " ha sacado un: $numal Total puntos ronda:" + jugadores[jugador].puntuacionronda)
-                    Toast.makeText(
-                        this@MainActivity,
-                        "OPPS!!! Ha salido un 1 El Jugador " + jugadores[jugador].numjugador + " Pierde su turno y puntos",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    cambiarTurno()
-                        binding.textView3.visibility = View.VISIBLE
-                    } else {
-                        jugadores[jugador].puntuacionronda += numal
-                        binding.textView3.setText("El " + jugadores[jugador].nombre + " ha sacado un: $numal Total puntos ronda: " + jugadores[jugador].puntuacionronda)
-                    }
-                    var puntuaciones: TextView = binding.puntuacion
-                    puntuaciones.textSize = 16F
-                    var mensajepuntos = ""
-                    for (i in 1..opcion) {
-                        mensajepuntos =
-                            mensajepuntos.plus("Puntos del " + jugadores[i - 1].nombre + ": " + jugadores[i - 1].puntuacion + "\n\n")
-                    }
-                    puntuaciones.setText(mensajepuntos)
-                    mensajepuntos = ""
-                    binding.button3.visibility = View.VISIBLE
-                if(ronda>5){
-                    binding.button3.visibility = View.GONE
-                    binding.textView3.visibility = View.GONE
-                }
-
-            }
-
-
-            botonplantarse.setOnClickListener {
-                Toast.makeText(
-                    this@MainActivity,
-                    "El Jugador" + jugadores[jugador].numjugador + " Se ha plantado",
-                    Toast.LENGTH_SHORT
-                ).show()
-                cambiarTurno()
             }
 
         },3000)}
-
-    fun cambiarTurno() {
-        jugadores[jugador].puntuacion+=jugadores[jugador].puntuacionronda
-        jugadores[jugador].puntuacionronda = 0
-        jugador += 1
-        if (jugador >= opcion) {
-            jugador = 0
-            ronda += 1
-            binding.textView4.setText("Numero de Ronda: $ronda")
-            binding.textView2.setText("${jugadores[jugador].nombre}")
-        }
-        if (ronda > maxRondas) {
-            binding.textView3.visibility = View.GONE
-            acabarPartida()
-        }
-        binding.textView2.setText("Turno del ${jugadores[jugador].nombre}")
-        binding.textView3.visibility = View.GONE
-    }
-
-    private fun acabarPartida() {
-        ocultarDado()
-        binding.button2.visibility = View.GONE
-        binding.button3.visibility = View.GONE
-        binding.textView2.visibility = View.GONE
-        binding.textView3.visibility = View.GONE
-        binding.imageView2.visibility = View.VISIBLE
-        var puntuaciones : TextView = binding.puntuacion
-        puntuaciones.textSize = 16F
-        var mensajepuntos=""
-        for(i in 1..opcion){
-            mensajepuntos = mensajepuntos.plus("Puntos del "+jugadores[i-1].nombre+": "+jugadores[i-1].puntuacion+"\n\n")
-        }
-        puntuaciones.setText(mensajepuntos)
-        mensajepuntos = ""
-        var max = 0
-        var ganador = 0
-        for(i in 1..opcion){
-            if(jugadores[i-1].puntuacion > max){
-                max = jugadores[i-1].puntuacion
-                ganador = jugadores[i-1].numjugador
-            }
-        }
-        if(ganador==0) {
-            binding.ganador.setText("Los jugadores no han obtenido ningun punto")
-            binding.textView4.setText("¡¡Partida Terminada!!")
-            binding.textView3.visibility = View.GONE
-        } else
-            binding.ganador.setText("Enhorabuena el ganador es: " + jugadores[ganador - 1].nombre + " con " + jugadores[ganador - 1].puntuacion + " puntos")
-            binding.textView4.setText("¡¡Partida Terminada!!")
-            binding.textView3.visibility = View.GONE
-
-    }
-
-    private fun ocultar() {
-        binding.button2.visibility = View.GONE
-        binding.textView2.visibility = View.GONE
-        binding.button3.visibility = View.GONE
-        binding.textView3.visibility = View.GONE
-        binding.imageView2.visibility = View.GONE
-    }
-
-    private fun ocultarDado(){
-        binding.imageView3.visibility = View.GONE
-        binding.imageView4.visibility = View.GONE
-        binding.imageView5.visibility = View.GONE
-        binding.imageView6.visibility = View.GONE
-        binding.imageView7.visibility = View.GONE
-        binding.imageView8.visibility = View.GONE
-    }
-
-
-    private fun mostrar() {
-        binding.button2.visibility = View.VISIBLE
-        binding.textView2.visibility = View.VISIBLE
-        binding.textView3.visibility = View.VISIBLE
-    }
 }
