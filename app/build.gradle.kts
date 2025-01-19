45decdb91b16467c06689f12dc1a93928bc4e4d2
Hugo <huguitronic17@gmail.com>
2025-01-19 21:27:18 +0100

    ultimo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,6 +65,8 @@ dependencies {
     implementation ("com.squareup.picasso:picasso:2.8")
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+
 
 
 
--- app/src/main/java/com/example/miprimerproyecto/firebaseapp/FbConAutG.kt
+++ app/src/main/java/com/example/miprimerproyecto/firebaseapp/FbConAutG.kt
@@ -1,6 +1,5 @@
 package com.example.miprimerproyecto.firebaseapp
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.miprimerproyecto.MainActivity
 import com.example.miprimerproyecto.R
-import com.example.miprimerproyecto.databinding.ActivityMainBinding
-import com.example.miprimerproyecto.databinding.ActivityMainFbBinding
-import com.example.miprimerproyecto.firebaseapp.FbConAutG.Companion.RC_SIGN_IN
+import com.example.miprimerproyecto.databinding.ActivityFbConAutGBinding
+import com.example.miprimerproyecto.databinding.ActivityFbSinAutBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -22,59 +19,53 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
-import com.squareup.picasso.Picasso
 
-
-class MainActivityFb : AppCompatActivity() {
-    lateinit var binding: ActivityMainFbBinding
+class FbConAutG : AppCompatActivity() {
+    lateinit var binding: ActivityFbConAutGBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainFbBinding.inflate(layoutInflater)
+        binding = ActivityFbConAutGBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_fb)
+        setContentView(R.layout.activity_fb_con_aut_g)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        auth = FirebaseAuth.getInstance()
-        val username = intent.getStringExtra("username")
-        val avatar = intent.getStringExtra("avatar")
-        Picasso.get().load(avatar).into(binding.imageView19)
-        binding.textView16.setText(binding.textView16.text.toString()+" "+username)
 
-        binding.button5.setOnClickListener(){
-            val intent = Intent(this@MainActivityFb, FbSinAut::class.java)
-            intent.putExtra("username", username)
-            intent.putExtra("avatar",avatar)
-            startActivity(intent)
-        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("791933835649-k2h6ndqsggp7iedv58ple2csqspfmeuk.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
 
-        binding.botongoogle.setOnClickListener(){
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // [END config_signin]
 
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("791933835649-k2h6ndqsggp7iedv58ple2csqspfmeuk.apps.googleusercontent.com")
-                .requestEmail()
-                .build()
-
-            googleSignInClient = GoogleSignIn.getClient(this, gso)
-            // [END config_signin]
-
-            // [START initialize_auth]
-            // Initialize Firebase Auth
-            startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
-
-        }
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
 
     }
 
+    // [START on_start_check_user]
+    /*
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+     */
+    // [END on_start_check_user]
+
+    // [START onactivityresult]
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -88,11 +79,50 @@ class MainActivityFb : AppCompatActivity() {
                         Log.d("hugo",it.result.user?.email.toString())
 
                     }
-                }
+
+}
+
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
             }
         }
+    }
+    // [END onactivityresult]
+/*
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(idToken: String) {
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+    // [END auth_with_google]
+
+
+ */
+    // [START signin]
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // [END signin]
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        const val RC_SIGN_IN = 9001
     }
 }
